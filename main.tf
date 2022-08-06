94938dea1c013d39e9830e085b53b92127985658
kirishima-sora <kirishima.sora.sinon@gmail.com>
2022-08-06 14:29:06 +0900

    new file IAM function

---
+++ main.tf
@@ -0,0 +1,95 @@
+
+#awsプロバイダ情報
+variable "aws_access_key_id" {}
+variable "aws_secret_access_key" {}
+provider aws {
+    access_key = var.aws_access_key_id
+    secret_key = var.aws_secret_access_key
+    region = "ap-northeast-1"   
+}
+
+#信頼ポリシー(lambdaがこのロールを受け取れるようにする)
+data aws_iam_policy_document assume_role {
+    statement {
+        actions = ["sts:AssumeRole"]
+        effect  = "Allow"
+        principals {
+            type = "Service"
+            identifiers = ["lambda.amazonaws.com"]
+        }
+    }
+}
+
+#IAMロールの作成
+resource aws_iam_role lambda_role {
+    name = "MyLambdaRole"
+    assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+#IAMポリシー（AWSLambdaBasicExecutionRole）
+data aws_iam_policy lambda_basic_execution {
+    arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+#IAMロールへのIAMポリシーのアタッチ
+resource aws_iam_role_policy_attachment lambda_basic_execution {
+    role = aws_iam_role.lambda_role.name
+    policy_arn = data.aws_iam_policy.lambda_basic_execution.arn
+}
+
+# #awsプロバイダ情報
+# provider aws {
+#     region = "ap-northeast-1"
+# }
+
+
+# #定数定義
+# locals {
+#     function_name  = "terraform_bangdre_function"
+# }
+
+
+# #KMSキー定義
+# resource aws_kms_key lambda_key {
+#     description  = "My Lambda Function Customer Master Key"
+#     enable_key_rotation = true
+#     deletion_window_in_days = 7
+# }
+
+# resource aws_kms_alias lambda_key_alias {
+#     name = "alias/my-lambda-key"
+#     target_key_id = aws_kms_key.lambda_key.id
+# }
+
+
+# #lambda実行ファイル定義
+# data archive_file function_source {
+#     type = "zip"
+#     source_dir = "app"
+#     output_path = "archive/lambda.zip"
+# }
+
+
+# #lambda作成
+# resource aws_lambda_function function {
+#     function_name = local.function_name
+#     handler = "lambda.handler"
+#     runtime = "python3.8"
+#     filename = data.archive_file.function_source.output_puth
+#     source_code_hash = data.archive_file.function_source.output_base64sha256
+#     role = aws_iam_role.lambda_role.arn
+#     kms_key_arn = aws_kms_key.lambda_key.arn
+#     environment {
+#         variables = {
+#             BASE_MESSAGE = "Hello"
+#         }
+#     }
+#     depends_on = [aws_iam_role_policy_attachment.lambda_policy, aws_cloudwatch_log_group.lambda_log_group]
+# }
+
+
+# #CloudWatchLogsグループ定義
+# resource "aws_cloudwatch_log_group" "lambda_log_group" {
+#     name = "/aws/lambda/${local.function_name}"
+# }
+
